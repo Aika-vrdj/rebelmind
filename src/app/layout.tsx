d963420e3a94e727fac28c45cbf4b2222436734d
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-03-15 18:38:06 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,18 @@ import { type Metadata } from "next";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "The Enneagram Hub",
+  title: "The Rebel Mind",
   description: "Explore and understand the nine Enneagram personality types",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [
+    { rel: "icon", url: "/favicon.ico", sizes: "16x16" },
+    { rel: "icon", url: "/favicon.ico", sizes: "32x32" },
+    { rel: "icon", url: "/favicon.ico", sizes: "48x48" },
+    { rel: "icon", url: "/favicon.png", type: "image/png", sizes: "64x64" },
+    // Optional for Apple devices
+    { rel: "apple-touch-icon", href: "/apple-touch-icon.png", sizes: "180x180" },
+    // Optional for Android devices
+    { rel: "icon", href: "/android-chrome-192x192.png", sizes: "192x192" },
+  ],
 };
 
 export default function RootLayout({
